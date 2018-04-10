6f19f5cc604deafcabfc74515143d6de998d11fd
Huaizheng <zhanghuaizheng0806@gmail.com>
2018-04-10 17:40:32 +0800

    create pages

--- demo.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 28/8/17
-# @Author  : Huaizheng ZHANG
-# @Site    : zhanghuaizheng.info
-# @File    : demo.py
--- models/LIVE_NFLX/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 8/3/18 10:50 PM
-# @Author  : Huaizheng Zhang
-# @Site    : zhanghuaizheng.info
-# @File    : __init__.py.py
